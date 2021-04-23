2a28a3f64bb285837ceefd9d5518469ce6fdd600
Ian Lambert <lambe347@umn.edu>
2021-04-23 15:43:46 -0500

    created database, added test data, added query to database.sql in vscode

---
+++ database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "tasks" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (250) NOT NULL,
+	"description" VARCHAR (300) NOT NULL,
+	"isComplete" BOOLEAN DEFAULT FALSE
+);
+
+SELECT * FROM "tasks";
+DROP TABLE;
+
+-- Test Value Entered
+INSERT INTO "tasks" 
+	("name", "description") VALUES ('Homework', 'Finish Assignment for Monday')  RETURNING "id", "name", "description", "isComplete";
