672b63211d2d52c431d6f867de24d84100e9b765
Ian Lambert <lambe347@umn.edu>
2021-04-24 17:02:05 -0500

    setup get request in router

---
+++ server/routers/tasks.router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const pool = require ('../modules/pool');
+
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "tasks";',;
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('error getting tasks', error);
+        res.sendStatus(500);
+    });
+});
