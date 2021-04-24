481df5b2e537368853d9251bd09b352fbd9624d9
Ian Lambert <lambe347@umn.edu>
2021-04-24 18:41:37 -0500

    get and post functions done in the router, moving on to client next

--- server/routers/tasks.router.js
+++ server/routers/tasks.router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Console } = require('node:console');
 const router = express.Router();
 const pool = require ('../modules/pool');
 
@@ -12,3 +13,17 @@ router.get('/', (req, res) => {
         res.sendStatus(500);
     });
 });
+router.post('/', (req, res) => {
+    let newTask = req.body;
+    console.log('Adding new task to the server', newTask);
+    let queryText = `INSERT INTO "tasks" ("name", "description")
+                    VALUES ($1, $2);`;
+    pool.query(queryTExt, [newTask.name, newTask.description])
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch(error => {
+            console.log('Error adding new task', error);
+            res.sendStatus(500);
+        })
+})
